6215125a905838912c978d57aa72ed428b5fe8c2
R.J Lelis <renatojlelis@gmail.com>
2018-05-24 10:01:38 -0300

    Update variable names & add fString

--- bin_to_decimal.py
+++ bin_to_decimal.py
@@ -2,16 +2,16 @@ def bin_to_decimal(binary):
     ''' Converts a binary number to decimal '''
     binary = str(binary)
     bits = []
-    b = 1
-    res = 0
-    for n in range(0, len(binary)):
+    bit = 1
+    for _ in range(0, len(binary)):
         bits.append(b)
-        b*=2
+        bit*=2
     bits.reverse()
+    res = 0
     for n in range(0, len(binary)):
         if binary[n] == "1":
             res += bits[n]
     return res
 
 number = input("Type a binary number to convert: ")
-print("{0} is {1} in decimal".format(number, bin_to_decimal(number)))
+print(f"{number} is {bin_to_decimal(number)} in decimal")
